75f1bb73175af043610993546cb7c95078ddd9e5
Ilya Bondarev <ilushkabond@gmail.com>
2022-05-03 01:10:57 +0200

    feat: chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chains: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (typeof value)
+      this.chains.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!Number.isInteger(position) || !(position > 0 && position < this.chains.length)) {
+      this.chains.length = 0;
+      throw Error('You can\'t remove incorrect link!');
+    }
+    this.chains.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this.chains.map(e => `( ${e} )`).join('~~');
+    this.chains.length = 0;
+    return chain;
   }
 };
 
