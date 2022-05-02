34498dc3f8b8fecc77fb212df991bb918633d92d
Ilya Bondarev <ilushkabond@gmail.com>
2022-05-03 01:29:22 +0200

    feat: transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const filtered = arr.filter((e, i) => arr[i + 1] !== '--discard-prev' && arr[i - 1] !== '--discard-next');
+  const mapped = filtered.map((e, i) => {
+    let result;
+    if (e === "--double-prev" && i > 0) {
+      result = arr[i - 1];
+    } else if (e === "--double-next" && i < arr.length - 1) {
+      result = arr[i + 1];
+    } else {
+      result = e;
+    }
+
+    return result;
+  });
+
+  return mapped.filter(e => !["--discard-prev", "--discard-next", "--double-prev", "--double-next"].includes(e));
 }
 
 module.exports = {
